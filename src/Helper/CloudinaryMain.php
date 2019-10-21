8c23e3d04106893ea9fbf5468f0539fd9638f4c8
ynswtf <senemyunus@gmail.com>
2019-10-21 11:23:33 +0300

    newpackage

--- src/Helper/CloudinaryMain.php
+++ src/Helper/CloudinaryMain.php
@@ -3,6 +3,7 @@ namespace SenemCO\CloudInary\Helper;
 
 use Pagekit\Application as App;
 use Cloudinary;
+use Mobile_Detect;
 
 class CloudinaryMain{
 
@@ -15,6 +16,14 @@ class CloudinaryMain{
     public function showImage( string $path = '' , array $options = []){
         self::config();
         return cloudinary_url( $path , $options );
+    }
+
+    public function typeDetect(){
+        $detect = new Mobile_Detect;
+        if($detect->isIphone()){
+            return 'jpg';
+        }
+        return 'webp';
     }
 
 }
